4540ecfd6d0c789527b8bdf4af5e99b69922fd33
Matheus Ribeiro <imlearningcodes@gmail.com>
2022-10-22 20:04:56 -0300

    PATCH request

--- main.go
+++ main.go
@@ -14,15 +14,15 @@ type  todo struct {
 }
 
 var todos = []todo{
-	{ID: "1", Item: "Clean Room", Completed: false},
-	{ID: "1", Item: "Clean Room", Completed: false},
-	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "1", Item: "Learning API", Completed: false},
+	{ID: "2", Item: "Create API", Completed: false},
+	{ID: "3", Item: "Test API", Completed: false},
 };
 
 func getTodos(context *gin.Context){
 	context.IndentedJSON(http.StatusOK, todos)
 }
-
+ 
 func addTodo(context *gin.Context){
 	var newTodo todo
 	if err := context.BindJSON(&newTodo); err != nil {
@@ -55,10 +55,25 @@ func getTodo(context *gin.Context){
 	context.IndentedJSON(http.StatusOK, todo); 
 }
 
+func toggleTodoStatus(context *gin.Context){
+	id := context.Param("id");
+	todo, err := getTodoById((id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	todo.Completed = !todo.Completed;
+	context.IndentedJSON(http.StatusOK, todo);
+}
+
+
 func main(){
 router := gin.Default();
 router.GET("/todos", getTodos)
 router.GET("/todos/:id", getTodo)
 router.POST("/todos", addTodo)
+router.PATCH("/todos/:id", toggleTodoStatus)
 router.Run("localhost:9090");
 }
