62459b3d9550af8511300c8f568e37704f58f297
Matheus Ribeiro <imlearningcodes@gmail.com>
2022-10-22 19:48:45 -0300

    GET by ID

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"errors"
 )
 
 type  todo struct {
@@ -31,9 +32,33 @@ func addTodo(context *gin.Context){
 
 	context.IndentedJSON(http.StatusCreated, newTodo);
 }
+
+func getTodoById(id string)(*todo, error){
+	for i, t := range todos {
+		if t.ID == id {
+			return &todos[i], nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
+}
+
+func getTodo(context *gin.Context){
+	id := context.Param("id");
+	todo, err := getTodoById((id))
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, todo); 
+}
+
 func main(){
 router := gin.Default();
 router.GET("/todos", getTodos)
+router.GET("/todos/:id", getTodo)
 router.POST("/todos", addTodo)
 router.Run("localhost:9090");
 }
